1bcb7285edf43348b3b0a02da1bbb824a8f8b58e
Junaid Oloruntobi <easytobeh@gmail.com>
2021-02-13 22:02:37 +0100

    Added Exception Handling

---
+++ ExceptionCustom/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+namespace ExceptionCustom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                throw new UserLoggedInException("User is logged in");
+            }
+            catch (UserLoggedInException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    [Serializable]
+    public class UserLoggedInException : Exception 
+    { 
+        public UserLoggedInException() : base()
+        {
+
+        }
+        public UserLoggedInException(string message) : base(message)
+        {
+
+        }
+
+        public UserLoggedInException(string msg, Exception innerException) 
+            : base(msg, innerException)
+        {
+
+        }
+
+        public UserLoggedInException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = new StreamReader(@"C:\Users\OloruntobiJunaid\Documents\The Fellowship\.Net\C#.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+                Console.WriteLine(streamReader.ReadToEnd());
+               
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("Please check if: " + ex.FileName + " exist");
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine($"Problem: {ex.Message}");
+            }
+
+            finally
+            {
+                streamReader.Close(); 
+                Console.WriteLine("Finally block");
+            }
+        }
+    }
+}
