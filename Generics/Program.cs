a310a9cc577911b061333a60c0a573ed83ba1f5e
Junaid Oloruntobi <easytobeh@gmail.com>
2021-02-14 14:27:31 +0100

    Playing with Genereics just got better

---
+++ Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool equal = Calculator<Type>.AreEqual("A", "A");
+
+            if(equal)
+                Console.WriteLine("Equal");
+            else
+                Console.WriteLine("Not Equal");
+        }
+    }
+
+    public class Calculator <Type>
+    {
+        public static bool AreEqual(Type value1, Type value2)
+        {
+            return value1.Equals(value2);
+        }
+    }
+}
